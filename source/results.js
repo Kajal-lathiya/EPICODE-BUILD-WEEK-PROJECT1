34a4af51c979edab84bd9b6ecdeae9e64ad8a657
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2022-10-19 14:55:27 +0200

    Update

--- source/results.js
+++ source/results.js
@@ -1,1 +1,4 @@
-const percent = document.getElementById;
+const circleContainer = document.getElementById("circlebox");
+const circleDiv = document.createElement("div");
+circleDiv.textContent = "hello";
+circleContainer.append(circleDiv);
