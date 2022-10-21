8701d80eb9b9f59ded6c431d9b66d84c91303818
Kajal <kajalbodarya@gmail.com>
2022-10-21 17:12:36 +0530

    add circle progressbar for show result

--- source/result.js
+++ source/result.js
@@ -16,20 +16,6 @@ const showResults = () => {
 rateUSButton.addEventListener('click', () => {
     location.href = 'review.html'
 })
-
-const numb = document.querySelector(".numb");
-numb.textContent = 100;
-
-let counter = 0;
-setInterval(() => {
-    if (counter == 100) {
-        clearInterval();
-    } else {
-        counter += 1;
-        numb.textContent = 100;
-    }
-}, 80);
-
 window.onload = () => {
     showResults();
 }
